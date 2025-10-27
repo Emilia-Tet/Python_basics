3cc5738b800aedda89b29dd395c8699666d44b4b
EMI_LAPTOP\emite <emi.tetmajer@gmail.com>
2025-10-27 19:40:17 +0100

    lekca Tadzia i Leosia 1

---
+++ lekcja 1.py
@@ -0,0 +1,14 @@
+from random import choice, shuffle
+
+print(900)
+print(8)
+
+moj_wybor = choice(range(1,90))
+print(moj_wybor)
+
+wybor_Tadzia = ['Wojanek','MiullerMilch','Snicers']
+print(wybor_Tadzia)
+print(choice(wybor_Tadzia))
+
+shuffle(wybor_Tadzia)
+print(wybor_Tadzia)
